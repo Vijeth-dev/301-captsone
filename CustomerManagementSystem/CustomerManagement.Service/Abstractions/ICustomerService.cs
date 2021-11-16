8a9548d7ca1dc95c8a7c2493024b70e79caa68e5
Vijeth <vijeths8@gmail.com>
2021-11-16 12:23:26 +0000

    First commit

---
+++ CustomerManagementSystem/CustomerManagement.Service/Abstractions/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomerManagement.Service.Abstractions
+{
+    public interface ICustomerService
+    {
+    }
+}
