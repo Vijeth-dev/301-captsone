7d278b7819377a5261142842d275b7c833a9caf5
Vijeth <vijeths8@gmail.com>
2021-11-17 11:54:20 +0000

    Customer DB created

---
+++ CustomerManagementSystem/CustomerManagement.Repo/Entities/Context/CustomerManagementContext.cs
@@ -0,0 +1,90 @@
+﻿using CustomerManagement.Repo.Entities.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomerManagement.Repo.Entities.Context
+{
+    public class CustomerManagementContext : DbContext
+    {
+        public CustomerManagementContext()
+        {
+        }
+
+        public CustomerManagementContext(DbContextOptions<CustomerManagementContext> options)
+            : base(options)
+        {
+        }
+        public string DB { get; set; }
+
+        public CustomerManagementContext(string connectionString)
+        {
+            DB = connectionString;
+        }
+        public virtual DbSet<TblCustomer> TblCustomer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Data Source=VSSqlKibana\\SQLEXPRESS;Initial Catalog=CustomerManagement;Integrated Security=True",b=>b.MigrationsAssembly("CustomerManagement.Repo"));
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+           
+
+            modelBuilder.Entity<TblCustomer>(entity =>
+            {
+                entity.ToTable("tblCustomer");
+
+                entity.Property(e => e.Id).HasColumnName("ID");
+
+                entity.Property(e => e.Address)
+                    .IsRequired()
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(225)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(225)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(225)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.MobileNumber)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.Password)
+                    .IsRequired()
+                    .HasMaxLength(225)
+                    .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.PasswordKey)
+                   .IsRequired()
+                   .HasMaxLength(225)
+                   .HasDefaultValueSql("('')");
+
+                entity.Property(e => e.CreatedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.ModifiedDate)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("((0))");
+            });
+        }
+    }
+}
---
+++ CustomerManagementSystem/CustomerManagement.Repo/Entities/Models/TblCustomer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CustomerManagement.Repo.Entities.Models
+{
+    public class TblCustomer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobileNumber { get; set; }
+        public string Password { get; set; }
+        public string PasswordKey { get; set; }
+        public string Address { get; set; }
+        public bool Active { get; set; }
+        public int Id { get; set; }
+        public int UserCreated { get; set; }
+        public int UserModified { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public int Totalorders { get; set; }
+
+    }
+}
